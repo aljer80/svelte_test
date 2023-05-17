acb04ee337fd3f9ccb84641a48c8ea821865025a
aljer80 <jernberg.alexandra@gmail.com>
2023-05-17 13:43:17 +0200

    imported todos from stores + Todo from components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<div>hi</div>
+<script>
+    import TodoForm from '../components/TodoForm.svelte';
+    import Todo from '../components/Todo.svelte';
+    import {todos} from '../stores/todoStore.js';
+    
+</script>
+
+<header> 
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</header>
+
+<main>
+    <h1>
+        My Todos
+    </h1>
+    <TodoForm/>
+    {#each $todos as todo}  <!-- $ in front of todos to access the data directly --> 
+        <Todo todo={todo} index={todo.id} />
+    {/each}
+</main>
